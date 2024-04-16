d71f910aa7a735a3e47603e5c3ae66560c56f347
Sachin <sachinbharti9891@gmail.com>
2024-04-17 01:34:32 +0530

    email password change successfully

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,5 +1,8 @@
 package com.driver;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Email {
 
     private String emailId;
@@ -18,7 +21,55 @@ public class Email {
         return password;
     }
 
+
+
+    public boolean checkValidCharacter(String newPassword)
+    {
+       boolean upperChar=false;
+       boolean lowerChar=false;
+       boolean digit=false;
+       boolean specialChar=false;
+
+        for(int i=0;i<newPassword.length();i++)
+        {
+            char ch=newPassword.charAt(i);
+            if(ch>='A' && ch<='Z')
+            {
+                upperChar=true;
+            }
+
+            if(ch>='a' && ch<='z')
+            {
+                lowerChar=true;
+            }
+
+            if(Character.isDigit(ch))
+            {
+                digit=true;
+            }
+            if(!Character.isDigit(ch) && !Character.isLetter(ch) && !Character.isWhitespace(ch))
+            {
+                specialChar=true;
+            }
+
+        }
+
+      if(newPassword.length()==8 && specialChar==true && digit==true && lowerChar==true && upperChar==true)
+      {
+          return true;
+      }
+      return false;
+    }
+
     public void changePassword(String oldPassword, String newPassword){
+        if(this.password.equals(oldPassword))
+        {
+            if(checkValidCharacter(newPassword))
+            {
+                this.password=newPassword;
+            }
+
+        }
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
